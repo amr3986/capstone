afd2d58a7d2d902187f7665298dd81ff4b48900c
Amr <amraljmal14@gmail.com>
2021-08-07 03:58:13 +0300

    new

--- starter/models.py
+++ starter/models.py
@@ -10,9 +10,9 @@ db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False    
     db.app = app
+    db.init_app(app)
     
 
 class Actor(db.Model):
